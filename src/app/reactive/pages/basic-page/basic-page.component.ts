a4fe075e19d83c44f75ae535a54f160fb545b52b
Cristian Delcid <cristian_delcid10@yahoo.com>
2023-03-15 00:29:12 -0600

    ReactiveForm agregados

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,6 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
 })
-export class BasicPageComponent {}
+export class BasicPageComponent implements OnInit {
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
+
+  public myForm: FormGroup = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.myForm.reset({ price: 0, inStorage: 0 });
+  }
+
+  onSave(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+    }
+
+    this.myForm.reset({ price: 0, inStorage: 0 });
+  }
+}
--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
 
@@ -11,6 +12,6 @@ import {
 
 @NgModule({
   declarations: [BasicPageComponent, DynamicPage, SwitchesPageComponent],
-  imports: [CommonModule, ReactiveRoutingModule],
+  imports: [CommonModule, ReactiveRoutingModule, ReactiveFormsModule],
 })
 export class ReactiveModule {}
