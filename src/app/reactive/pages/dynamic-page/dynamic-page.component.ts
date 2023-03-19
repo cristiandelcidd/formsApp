da3e93adaed8050c497c88be1f4ad3e3b11d032c
Cristian Delcid <cristian_delcid10@yahoo.com>
2023-03-19 01:26:44 -0600

    Formularios Reactivos

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,6 +1,60 @@
 import { Component } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
 })
-export class DynamicPage {}
+export class DynamicPage {
+  public myForm: FormGroup = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteMovies: this.formBuilder.array([
+      ['Harry Potter', Validators.required],
+      ['Spiderman', Validators.required],
+    ]),
+  });
+
+  public newFavorite: FormControl = new FormControl('', [Validators.required]);
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  get favoriteMovies(): FormArray<any> {
+    return this.myForm.get('favoriteMovies') as FormArray;
+  }
+
+  onDeleteFavorite(index: number): void {
+    this.favoriteMovies.removeAt(index);
+  }
+
+  onAddToFavorites(): void {
+    if (this.newFavorite.invalid) return;
+
+    this.favoriteMovies.push(
+      this.formBuilder.control(this.newFavorite.value, Validators.required)
+    );
+
+    this.newFavorite.reset();
+  }
+
+  isValidField(field: string): boolean | null {
+    // prettier-ignore
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
+
+  onSubmit(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    (this.myForm.controls['favoriteMovies'] as FormArray) =
+      this.formBuilder.array([]);
+
+    this.myForm.reset();
+  }
+}
--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,6 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
 })
-export class SwitchesPageComponent {}
+export class SwitchesPageComponent implements OnInit {
+  public myForm: FormGroup = this.formBuilder.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
+
+  public person = { gender: 'F', wantNotifications: false };
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.myForm.reset(this.person);
+  }
+
+  onSave(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    const { termsAndConditions, ...newPerson } = this.myForm.value;
+
+    this.person = newPerson;
+  }
+
+  isValidField(field: string): boolean | null {
+    // prettier-ignore
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
+}
